cf664b5150fa6f3289f82b23b5ffc2f29d70db37
sumanthsvjfrog <sumanthsv@jfrog.com>
2026-04-30 12:33:47 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,6 +8,26 @@ var DB_PASSWORD = "SuperSecret123!";
 var API_KEY = "API_KEY_XYZ123";
 var JWT_SECRET = "fakejwtsecret456";
 
+// ❌ Additional hardcoded passwords — various patterns SAST tools detect
+var adminPassword     = "admin1234!";
+var rootPassword      = "r00tP@ssw0rd";
+var dbConnectionStr   = "mysql://root:P@ssw0rd99@localhost:3306/mydb";
+var mongoUri          = "mongodb://admin:Mongo$ecret42@mongo.internal:27017/prod";
+var ldapBindPassword  = "LdapB!nd2024";
+var encryptionKey     = "aes256key-hardcoded-do-not-use";
+var privateKeyPem     = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEA0Z3VS5JJcds3xHn/ygWep4\n-----END RSA PRIVATE KEY-----";
+var awsSecretKey      = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+var githubToken       = "ghp_A1b2C3d4E5f6G7h8I9j0K1l2M3n4O5p6Q7r8";
+var slackWebhook      = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
+
+// ❌ Password reuse via config object
+var dbConfig = {
+  host:     "prod-db.internal",
+  user:     "dbadmin",
+  password: "Pr0d-DB-P@ss!",
+  database: "customers"
+};
+
 // ❌ Catastrophic regex — ReDoS (CWE-1333)
 var EMAIL_REGEX = /^([a-zA-Z0-9]+)*@([a-zA-Z0-9]+\.)+[a-zA-Z]{2,}$/;
 
@@ -38,6 +58,9 @@ http.createServer(function(req, res) {
     var data = fs.readFileSync('/var/data/' + filePath);
     res.write(data);
   }
+
+  // ❌ Password logged to console (CWE-312)
+  console.log("Connecting with password: " + dbConfig.password);
 
   // ❌ XSS — reflecting unsanitized input into HTML (CWE-79)
   res.end('<html><body><p>Result: ' + result + '</p><p>Input was: ' + userInput + '</p></body></html>');
